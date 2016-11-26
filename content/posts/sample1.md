4bb8a5819759744a7e367b785742a747095135d5
miyuchina <miyu.china@icloud.com>
2016-11-26 17:01:50 -0500

    restructure files to satisfy flask requirements

--- content/posts/sample1.md
+++
@@ -1,5 +0,0 @@
-title: Sample Post 1
-author: James Bond
-date: 10-21-2011
----
-I have returned from a mission in *Egypt*.
---
+++ dump.py
@@ -0,0 +1,78 @@
+# Python packages
+import os, shutil
+import http.server, socketserver
+
+# User-defined packages
+import blog, page
+
+# External packages
+from jinja2 import Environment, FileSystemLoader
+
+
+def dump_index(env, site_obj, included_posts):
+    """
+    Generate static pages for index.
+
+    Args:
+        env: the template environment.
+        site_obj: a site object.
+        included_posts: a list of included posts.
+        static_dir: the output directory.
+    """
+    template = env.get_template('index.html')
+    index_style = "css/index.css"
+
+    with open(site_obj.get_static_dir() + "index.html", "w+") as f:
+        f.write(template.render(site=site_obj, posts=included_posts, style=index_style))
+
+def dump_post(env, site_obj, post_obj):
+    """
+    Generate static pages for posts.
+
+    Args:
+        env: the template environment.
+        site_obj: a site object.
+        post_obj: a page object with page_type = "post".
+        static_dir: the output directory.
+    """
+    template = env.get_template('post.html')
+    post_style = "../css/post.css"
+
+    static_name = "{} - {}.html".format(post_obj.get_date(), post_obj.get_title())
+    with open(site_obj.get_static_dir() + "posts/" + static_name, "w+") as f:
+        f.write(template.render(site=site_obj, post=post_obj, style=post_style))
+
+def dump_css(env, site_obj):
+    css_static_dir = site_obj.get_static_dir() + "css/"
+
+    for css_file in os.listdir(site_obj.get_theme_dir()):
+        shutil.copy(site_obj.get_theme_dir() + css_file, css_static_dir)
+
+
+def generate():
+    """
+    Generate all static pages.
+    """
+    # Create Blog, Environment and Post objects.
+    i = page.Index()
+    b = blog.Blog()
+    env = Environment(loader=FileSystemLoader(b.get_templates_dir()))
+
+    included_posts = []
+
+    # Generate all posts.
+    for md_file in os.listdir(b.get_posts_dir()):
+        if b.check_included(os.path.basename(md_file)):
+            p = page.Post()
+            p.load(b.get_posts_dir() + md_file)
+            dump_post(env, b, p)
+            included_posts.append(p)
+
+    # Generate index.
+    dump_index(env, b, included_posts)
+
+    # Generate css.
+    dump_css(env, b)
+
+if __name__ == "__main__":
+    generate()
