7da9e93dbba2ec24ee7e47861374a79b5a25bf88
miyuchina <miyu.china@icloud.com>
2016-11-27 10:31:14 -0500

    implemented basic flask functions

--- server.py
+++ server.py
@@ -10,3 +10,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+@app.route('/css/<css_file>')
+def css(css_file):
+    return app.send_static_file("css/" + css_file)
+
+@app.route('/posts/<post_name>')
+def post(post_name):
+    return app.send_static_file("posts/" + post_name)
