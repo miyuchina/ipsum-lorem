284522d887eb8bfd6215e94d1a9bec0b2e2eb10d
miyuchina <miyu.china@icloud.com>
2016-11-29 11:41:46 -0500

    optimize push to gh-pages workflow

--- github.py
+++ github.py
@@ -1,8 +1,10 @@
 import os
 
-def to_gh_pages():
+def to_gh_pages(commit_msg):
     """
     """
+    os.system("git commit -m {} -a".format(commit_msg))
+    os.system("git push")
     os.system("git subtree push --prefix static origin gh-pages")
     
     print("Dumped to Github Pages.")
--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import os, sys
 import dump, github, server
 
 def usage():
-    print("Usage: python main.py [github|local]")
+    print("Usage: python main.py [github|local] <commit-msg>")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2 or (sys.argv[1] == "github" and len(sys.argv) < 3):
         usage()
     else:
         if sys.argv[1] == "github":
             dump.generate(sys.argv[1])
-            github.to_gh_pages()
+            github.to_gh_pages(" ".join(sys.argv[2:]))
         elif sys.argv[1] == "local":
             dump.generate(sys.argv[1])
             server.app.run()
