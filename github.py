401179973d3c0cb96185be35cfa7a5f0e9af2b2d
miyuchina <miyu.china@icloud.com>
2016-11-30 12:46:39 -0500

    revert quiet flag

--- github.py
+++ github.py
@@ -12,14 +12,14 @@ def to_gh_pages(commit_msg, site_obj):
         commit_msg: the commit message.
     """
     # make sure all files are added
-    os.system("git add --quiet *")
+    os.system("git add *")
 
     # commit and push to main branch
-    os.system("git commit -m '{}' -a --quiet".format(commit_msg))
-    os.system("git push --quiet")
+    os.system("git commit -m '{}' -a".format(commit_msg))
+    os.system("git push")
 
     # push to gh-pages branch
-    os.system("git subtree push --prefix static origin gh-pages --quiet")
+    os.system("git subtree push --prefix static origin gh-pages")
 
     term_prompt_header = colored("[{}] ".format(site_obj.name), "cyan")
     print(term_prompt_header + "Dumped to Github Pages.")
